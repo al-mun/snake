20984660b43c9f2e2524d9439bd2b65e787f09d6
Alejandro <63469503+alejandro-mun@users.noreply.github.com>
2021-11-14 15:45:03 -0500

    Add files via upload

--- draw.js
+++ draw.js
@@ -1,44 +1,68 @@
+
 const canvas = document.querySelector(".canvas");           //Find the class canvas
 const ctx = canvas.getContext("2d");                    //get canvas context
 const scale = 20;                                       //rows and collumns are 10 pixels
 const rows = canvas.height / scale;                     //row are the height divided by 10
 const columns = canvas.width / scale;                   // collumns are the width divided by 10
+var snake;                                      //initialize snake, which will be equal to Snake Function     
+const upBtn = document.querySelector(".up")
+const downBtn = document.querySelector(".down")
+const leftBtn = document.querySelector(".left")
+const rightBtn = document.querySelector(".right")
 
-var snake;                                      //initialize snake     
 
+function game(){
 (function setup() {                             //initialize
     snake = new Snake();                        //snake 
     fruit = new Fruit();                        //fruit
-
     fruit.pickLocation();                       //makes the fruit pick random location!
     //console.log(fruit);
-
     window.setInterval(() => {
-
-
 
         ctx.clearRect (0, 0, canvas.width, canvas.height);      //refresh properly
         fruit.draw();                                           //draw the fruit
         snake.update();                                         //Update the snake's position, and tail
         snake.draw();                                           //draw the snake
+        
 
-
-        if (snake.eat(fruit)){  
-                                                                //if statement, if snake eats parameter fruit 
+        if (snake.eat(fruit)){                               //if statement, if snake eats parameter fruit 
             fruit.pickLocation();                                   // fruit picks a new location to appear.
         }
-
         snake.checkCollision();                                     //snake will check if there is collision.
-        document.getElementsByClassName(".score").innerText = snake.total;   //change the "score" in HTML to snake.total.
+        document.querySelector(".score").innerText = snake.total;   //change the "score" in HTML to snake.total.
     }, 150);                // times a second draw and update everything
+    
 }());
-
-
 
 window.addEventListener('keydown', ((evt) => {          //add listener(when key is pressed(call it evt)) function 
     console.log(evt);
-    const direction = evt.key.replace('Arrow', '');      //
-    //console.log(direction);
-    snake.changeDirection(direction);                    //make snake change direction, with direction being the key pressed by user
+  const direction = evt.key.replace('Arrow', '');      //
+    // console.log(direction);
 
+   snake.changeDirection(direction);                    //make snake change direction, with direction being the key pressed by user
+   
 }))
+
+upBtn.addEventListener("click", ((evt)=>{
+    let upString = evt.pointerId.toString()
+    const direction = upString.replace("1", "Up")
+    snake.changeDirection(direction)
+}))
+downBtn.addEventListener("click", ((evt)=>{
+    let downString = evt.pointerId.toString()
+    const direction = downString.replace("1", "Down")
+    snake.changeDirection(direction)
+}))
+leftBtn.addEventListener("click", ((evt)=>{
+    let leftString = evt.pointerId.toString()
+    const direction = leftString.replace("1", "Left")
+    snake.changeDirection(direction)
+}))
+rightBtn.addEventListener("click", ((evt)=>{
+    let rightString = evt.pointerId.toString()
+    const direction = rightString.replace("1", "Right")
+    snake.changeDirection(direction)
+}))
+
+
+}
--- snake.js
+++ snake.js
@@ -4,70 +4,102 @@ function Snake(){                   //Need to say this because it's refering to 
     this.xSpeed = scale * 1;        //initialize the x speed of the snake: move 10px or as fast as scale
     this.ySpeed = 0;
     this.total = 0;
-    this.tail = [];  //store coordinates
+    this.tail = [];  //
 
     this.draw = function() {            //
-        ctx.fillStyle = "black";      //make snake black
-
+        ctx.fillStyle = "black";      //draw snake as black
+        ctx.fillRect(this.x, this.y, scale, scale);      //position snake at 00 with width/height same size as rows/collumns        
         for (let i=0; i<this.tail.length; i++){                         //for loop - let i start at 0, while i is less than tail length, increment
-            ctx.fillRect(this.tail[i].x, this.tail[i].y, scale, scale);     //make the tail at coordinates located behind the rect.
+            ctx.fillRect(this.tail[i].x, this.tail[i].y, scale, scale);     //draw the tail at (x,y,width,height)  
         }
-        ctx.fillRect(this.x, this.y, scale, scale);         //Return snake to start
     }
  
     this.update = function() {                                  //update function
-        for(let i=0; i<this.tail.length -1; i++){               //for loop, 
-            this.tail[i] = this.tail[i +1];
+        for(let i=0; i<this.tail.length - 1; i++){               //for loop, as long as i is less than the length of the tail -1, increase
+            this.tail[i] = this.tail[i + 1];                //
         }
-        this.tail[this.total - 1] = {x: this.x, y: this.y};
+        
+        this.tail[this.total - 1] = {x: this.x, y: this.y};     //tail[-1] = x: x, y: y
+        //console.log(this.tail);
+        this.x += this.xSpeed;              //make snake's x coordinate += scale * 1 
+        this.y += this.ySpeed;              //make snake's y coordinate += 0
 
-        this.x += this.xSpeed;              
-        this.y += this.ySpeed;
-
-        if (this.x > canvas.width){
-            this.x = 0;
+        if (this.x > canvas.width){             //if x position is greater than the width of canvas (400)
+            this.x = 0;                                 //x coordinate is back at 0
         }
-        if (this.y >= canvas.height){
-            this.y = 0;
+        if (this.y >= canvas.height){           //if y coordinate is >= the height of canvas (400)
+            this.y = 0;                                 //y coordinate is 0
         }
-        if (this.x < 0){
-            this.x = canvas.width;
+        if (this.x < 0){                            //if x is less than 0 (0,0 is at top left corner)
+            this.x = canvas.width;                      //x coordinate is canvas.width (400) make it go to the right side
         }
-        if (this.y < 0){
-            this.y = canvas.height;
+        if (this.y < 0){                            //if y is less than 0, (means it's going up)
+            this.y = canvas.height;                             //y = canvas heigtht (400, all the way at the bottom)
         }
     }
-
-this.changeDirection = function(direction){
-    switch (direction){
-      case 'Left':
-        if(!this.xSpeed > 0){           
-            this.xSpeed = -scale * 1;
-            this.ySpeed = 0;
-        }
-        break;
-      case 'Up':
-        if(!this.ySpeed > 0 ){           //if going anywhere but down, go up
-            this.xSpeed = 0;               //x-axis = 0
-            this.ySpeed = -scale * 1;      //y-axis = go up
-        }
-           break;
-      case 'Right':
-        if(!this.xSpeed > 0){
-            this.xSpeed = scale *1;
-            this.ySpeed = 0;
-        }
-        break;
-       case 'Down':                     //need to be going up to cancel down
-        if(!this.ySpeed > 0){           //if going anywhere but up, go down
-            this.xSpeed = 0;            
-            this.ySpeed = scale *1;
-        }
-    }
-    }
-
     
 
+    this.changeDirection = function (direction) {
+        //now there are 2 switch statements, with an "if". One for the start of the game, the other for any tail longer than 0.
+        ///////////////////////////////////
+        if (this.tail.length == 0) { ///if it is only one block, at game start, allow movemet in all directions
+          //console.log(this.tail.length);
+          switch (direction) {
+            case 'Left':
+              //console.log(this.xSpeed);
+              if (this.xSpeed >= 0) {
+                this.xSpeed = -scale * 1;
+                this.ySpeed = 0;
+                //console.log(this.xSpeed);
+              }
+              break;
+            case 'Up':
+              if (this.ySpeed >= 0) { //if going anywhere but down, go up
+                this.xSpeed = 0; //x-axis = 0
+                this.ySpeed = -scale * 1; //y-axis = go up
+              }
+              break;
+            case 'Right':
+              if (this.xSpeed <= 0) {
+                this.xSpeed = scale * 1;
+                this.ySpeed = 0;
+                //	console.log(this.xSpeed);
+              }
+              break;
+            case 'Down': //need to be going up to cancel down
+              if (this.ySpeed <= 0) { //if going anywhere but up, go down
+                this.xSpeed = 0;
+                this.ySpeed = scale * 1;
+              }
+          } //end of switch
+        } else { //the tail is not 0 anymore, it is longer....
+      
+          switch (direction) { ////the old, "regular" speed switch statement
+            case 'Left':
+              if (!this.xSpeed > 0) {
+                this.xSpeed = -scale * 1;
+                this.ySpeed = 0;
+              }
+              break;
+            case 'Up':
+              if (!this.ySpeed > 0) { //if going anywhere but down, go up
+                this.xSpeed = 0; //x-axis = 0
+                this.ySpeed = -scale * 1; //y-axis = go up
+              }
+              break;
+            case 'Right':
+              if (!this.xSpeed > 0) {
+                this.xSpeed = scale * 1;
+                this.ySpeed = 0;
+              }
+              break;
+            case 'Down': //need to be going up to cancel down
+              if (!this.ySpeed > 0) { //if going anywhere but up, go down
+                this.xSpeed = 0;
+                this.ySpeed = scale * 1;
+              }
+          }
+        }
 
 
     this.eat = function(fruit){                         //eat function(parameter: fruit)
@@ -91,4 +123,4 @@ this.changeDirection = function(direction){
                 else{
                     location.reload();
                 }
-}}}}
+}}}}}
